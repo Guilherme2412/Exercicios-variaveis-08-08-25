3cc98612d61dcc22844472d194ce87a3cd8b440f
Guilherme Lazarini <091969@PROWAY.TREINA>
2025-08-08 19:58:15 -0300

    Atividade nova criada

---
+++ exercicio01.js
@@ -0,0 +1,7 @@
+function exercicio01(){
+    let nome = "Guilherme";
+    let idade = 20;
+    let altura = 1.78;
+
+    console.log("Olá meu nome é", nome, "minha idade é", idade, "e minha altura é", altura );
+}
