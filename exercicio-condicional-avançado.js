f13eedc5efdae6947f54c8b9f3520c5a9a04864d
YukiNagashi090 <guilh3rm3l2z2@gmail.com>
2025-08-17 14:37:00 -0300

    Arquivo atualizado

--- exercicio-condicional-avançado.js
+++ exercicio-condicional-avançado.js
@@ -3,22 +3,45 @@ function exercicio01 (){
     let nota2 = parseFloat(prompt("Digite a segunda nota").replace(",", "."));
     let nota3 = parseFloat(prompt("Digite a terceira nota").replace(",", "."));
     let mediaPonderada = (nota1 * 2 + nota2 * 3 + nota3 * 5) /10
-    let mediaPosExame = (mediaPonderada + exame) /2
+
 
     if (mediaPonderada >= 7){
         console.log("Aprovado")
-    }else if(mediaPonderada > 5 && mediaPonderada < 6.8){
-        console.log("Recuperação")
-    }else if(mediaPonderada >= 6.8 && mediaPonderada <= 6.99){
-        
-        
-        let trabalhoExtra = prompt("Fez trabalho extra? 'sim ou não' ")
-        if(trabalhoExtra === sim){mediaPonderada + 0.2; 
-            console.log("Aprovado")}
-            else if(trabalhoExtra === não){
-                if()
-            }
+        return;
     }
 
+    if (mediaPonderada >= 5 && mediaPonderada < 7) {
+        if (mediaPonderada >= 6.8 && mediaPonderada <= 6.99) {
+            let trabalhoExtra = prompt("Fez trabalho extra? (sim ou não)").trim().toLowerCase();
+        
+        if(trabalhoExtra === "sim"){
+            mediaPonderada += 0.2; 
+            if(mediaPonderada > 10) mediaPonderada = 10;
+            console.log("Bônus aplicado. Nova média: " + mediaPonderada.toFixed(2));
+        }else {
+            console.log("Sem trabalho extra. Média permanece" + mediaPonderada.toFixed(2));
+        }
+        
+    }
+
+    if(mediaPonderada >= 7) {
+        console.log("Aprovado");
+    } else {
+        let exame = parseFloat(prompt("Digite sua nota do Exame").replace(",", "."));
+        let mediaPosExame = (mediaPonderada + exame) /2;
+
+        if(mediaPosExame >= 6) {
+            console.log("Aprovado em exame");
+        }else {
+            console.log("Reprovado")
+        }
+    }
+    return;
+
+
+    console.log("Reprovado" + "Aprovado" + "Reprovado em Exame")
+    
+
+    }
 
 }
