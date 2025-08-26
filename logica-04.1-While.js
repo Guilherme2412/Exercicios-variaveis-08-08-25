53adbfd70409010280c1fe3419ae1b747f74e4c6
Guilherme Lazarini <091969@PROWAY.TREINA>
2025-08-25 21:51:55 -0300

    Arquivos modificados

---
+++ logica-04.1-While.js
@@ -0,0 +1,19 @@
+function comprasComPrecoBR() {
+    let indice = 0;
+    let total = 3;
+    let maiorPreco = 0;
+    let produtoMaisCaro = 0;
+    let quantidade = 0;
+    let produto = "";
+    let preco = 0;
+    let desejaCadastrar = "";
+
+    while(desejaCadastrar === "sim"){
+        desejaCadastrar = prompt("Deseja cadastrar?")
+    }
+{
+    let produto = prompt("digite o nome do produto".trim(length >= 2))
+     parseFloat((produto === "R$".replace("R$", " ", ". ", ",", ".")))
+}
+
+}
